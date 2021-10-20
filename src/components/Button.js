556ed4a22ea82054981277f745122fa6e34dd894
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-20 13:26:52 +0200

    00003 - prepare demo

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+  clickHandler = () => this.setState((pervState) => ({ counter: pervState.counter + 1 }));
   render() {
-    return (<button>Hello...</button>);
+    return <button onClick={this.clickHandler}>{this.state.counter}</button>;
   }
 }
 
