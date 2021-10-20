22140ef233ca052339977c426ee397c726a41bd3
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-20 11:53:34 +0200

    00002 - prepare demo

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,8 @@
-const Button = function(){
-    return(
-        <button>Hello...</button>
-    )
+import React, { Component } from "react";
+class Button extends Component {
+  render() {
+    return (<button>Hello...</button>);
+  }
 }
 
 export default Button;
