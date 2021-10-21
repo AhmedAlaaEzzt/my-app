bd9fbe89371916066a2133580fa61ed0a73d3ead
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-21 23:50:19 +0200

    00006 - prepare demo

---
+++ src/components/card/card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img alt="user" src="http://placehold.it/75" />
+      <div>
+        <div>Name here...</div>
+        <div>Company Name here ...</div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
