cd7eb9f30e7271bbaa7c8033d47297bfbb77978c
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-22 11:09:11 +0200

    00009 - add from to our demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import CardList from './Components/card-list/card-list'
-import data from './data/face-data';
+import "./App.css";
+import CardList from "./Components/card-list/card-list";
+import data from "./data/face-data";
+import Form from "./Components/form/From";
 function App() {
   return (
     <div className="">
-      <div>
-        <CardList cards={data}/>
+      <div style={{margin:"20px"}}>
+        <Form />
+        <CardList cards={data} />
       </div>
     </div>
   );
