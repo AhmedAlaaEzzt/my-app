3edc3aecff228d67b32610b64d75a4724277f2c0
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-22 12:00:26 +0200

    00010 - prepare demo

--- src/Components/form/From.js
+++ src/Components/form/From.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 
 class Form extends Component {
+    handleSubmit=(event)=>{
+        event.preventDefault();
+        console.log(`Event: from submit`);
+    }
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit} >
         <input type="text" placeholder="User name" required />
         <button type="submit">Add card</button>
       </form>
