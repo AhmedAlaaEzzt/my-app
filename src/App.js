0f975babce751e60837a2b7fd564a5bd8c87dbf1
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-24 19:31:14 +0200

    00038 - prepare demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import CardList from "./components/card-list/card-list";
+// import { robots } from "./Data/robots";
+import SearchBox from "./components/search-box/search-box";
+import { Component } from "react";
+import "./App.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log("constructor");
+  }
+
+  componentDidMount(){
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then(response =>{
+         return response.json();
+      })
+      .then(users => {
+        setTimeout(()=>this.setState({robots: users}), 3000)
+        
+      })
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+
+  };
+
+  render() {
+    console.log("render");
+    const filteredRobots = this.state.robots.filter((robot) => {
+      if (robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()))
+        return robot;
+    });
+    if(this.state.robots.length){
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }else{
+      return (<h1 className="tc">Loading.......</h1>)
+    }
+
+  }
 }
 
 export default App;
---
+++ src/components/search-box/search-box.js
@@ -0,0 +1,9 @@
+const SearchBox = (props) => {
+  return (
+    <div className="pa2">
+      <input onChange={props.searchChange} className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robots" />
+    </div>
+  );
+};
+
+export default SearchBox;
