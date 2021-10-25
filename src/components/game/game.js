ec225c2de0143b090be3ce588643696b922b6340
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-25 13:31:12 +0200

    00017 - prepare demo

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Stars from "../stars/stars.js";
 import Button from "../button/button.js";
 import Answer from "../answer/answer.js";
+import Numbers from "../numbers/numbers.js";
 
 class Game extends Component {
   render() {
@@ -14,6 +15,8 @@ class Game extends Component {
           <Button />
           <Answer />
         </div>
+        <br />
+        <Numbers />
       </div>
     );
   }
---
+++ src/components/numbers/numbers.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Numbers = function() {
+  return (
+    <div className="card text-center">
+      <div>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+      </div>
+    </div>
+  );
+};
+
+export default Numbers;
