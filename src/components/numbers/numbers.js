13866041c5e1c4fadfdb6f8421b46a58c679d131
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-27 10:06:58 +0200

    00021 - prepare demo

--- src/components/numbers/numbers.js
+++ src/components/numbers/numbers.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./numbers.css";
-const Numbers = function() {
+import { range } from "lodash";
+const Numbers = function () {
+  const numberList = range(1,10)
+
   return (
     <div className="card text-center">
       <div>
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
+        {numberList.map((number, i) => (
+          <span key={i}>{number}</span>
+        ))}
       </div>
     </div>
   );
