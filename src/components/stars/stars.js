a3d644c51ad57f744763aad5f86e7fbe6d82b75b
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-27 07:57:21 +0200

    00020 - prepare demo

--- src/components/stars/stars.js
+++ src/components/stars/stars.js
@@ -2,15 +2,12 @@ import React from "react";
 import "font-awesome/css/font-awesome.min.css";
 import "./stars.css";
 const Stars = function() {
-  return (
-    <div className="col-5">
-      <i class="fa fa-star" aria-hidden="true" />
-      <i class="fa fa-star" aria-hidden="true" />
-      <i class="fa fa-star" aria-hidden="true" />
-      <i class="fa fa-star" aria-hidden="true" />
-      <i class="fa fa-star" aria-hidden="true" />
-    </div>
-  );
+  const numberOfStars = Math.trunc(Math.random() * 9) + 1;
+  let stars = [];
+  for (let i = 0; i < numberOfStars; i++) {
+    stars.push(<i key={i} className="fa fa-star" aria-hidden="true" />);
+  }
+  return <div className="col-5">{stars}</div>;
 };
 
 export default Stars;
