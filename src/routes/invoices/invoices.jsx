349b5cb092be3224af7e5abd5d06cf4c6c168e4e
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-05 22:13:40 +0200

    00048 - prepare demo

--- src/routes/invoices/invoices.jsx
+++ src/routes/invoices/invoices.jsx
@@ -1,12 +1,19 @@
-import {getInvoices} from '../../database/invoices.js';
-import './invoices.css';
+import { getInvoices } from "../../database/invoices.js";
+import "./invoices.css";
+import { Link } from "react-router-dom";
 export default function Invoices() {
   let invoices = getInvoices();
   return (
     <div className="invoices-list">
       <nav>
-        {invoices.map(invoice =>(
-          <div className='invoice'>{invoice.name}</div>
+        {invoices.map((invoice) => (
+          <Link
+            className="invoice"
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </Link>
         ))}
       </nav>
     </div>
