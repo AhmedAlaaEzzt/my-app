a89b5105904cc5889dcdeb6bab1705517da9beec
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-05 19:51:19 +0200

    00045 - prepare the demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="book-keeper">
       <h1>Bookkeeper!</h1>
+      <Link to="/invoices">Invoices</Link> |{" "}
+      <Link to="/expenses">Expenses</Link>
     </div>
   );
 }
