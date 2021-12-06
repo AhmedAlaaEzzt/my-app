3245373e138aeef0c68cd1514ce6c24449e6237f
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-06 12:53:31 +0200

    00050 - prepare demo

---
+++ src/routes/invoice-placeholder/invoice-placeholder.jsx
@@ -0,0 +1,13 @@
+import "./invoice-placeholder.css";
+
+const InvoicePlaceholder = () => {
+  return (
+    <div className='invoice-placeholder'>
+      <main >
+        <p>Select an invoice</p>
+      </main>
+    </div>
+  );
+};
+
+export default InvoicePlaceholder;
