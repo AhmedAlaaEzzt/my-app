e76d2dc18030551c1b3319368baa5d916798641c
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-06 17:35:02 +0200

    00053 - prepare demo

--- src/routes/invoices/invoices.jsx
+++ src/routes/invoices/invoices.jsx
@@ -1,20 +1,45 @@
 import { getInvoices } from "../../database/invoices.js";
 import "./invoices.css";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
+
 export default function Invoices() {
   let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  const handleChange = (event) => {
+    let filter = event.target.value;
+    if (filter) {
+      setSearchParams({ filter });
+    } else {
+      setSearchParams({});
+    }
+  };
+
   return (
     <div className="invoices-list">
       <nav>
-        {invoices.map((invoice) => (
-          <NavLink
-            className="invoice"
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        <input
+          value={searchParams.get("filter") || ""}
+          onChange={handleChange}
+        />
+
+        {invoices
+          .filter((invoice) => {
+            let filter = searchParams.get("filter");
+            if (!filter) return true;
+
+            return invoice.name.toLowerCase().includes(filter.toLowerCase());
+          })
+          .map((invoice) => (
+            <NavLink
+              className="invoice"
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
+          
       </nav>
       <Outlet />
     </div>
