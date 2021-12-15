aa4ccea5f3951cd9c937f2cd83318f696318dadd
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-15 18:40:32 +0200

    00057 - prepare demo

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import "./App.css";
 import About from "./pages/about/about.page";
 import Users from "./pages/users/users.page";
 import Home from "./pages/home/home.page";
-
+import { BrowserRouter,Switch,Route } from "react-router-dom";
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <BrowserRouter>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
