62a1af05a4101f757511a8fa21aaf5fb5dca83c8
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-16 20:15:24 +0200

    00060 - prepare demo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import ContactList from "./components/contact-list/contact-list.component";
-// import contactsData from "./data/contacts.data"; //fake data
+import contactsData from "./data/contacts.data"; //fake data
 import * as ContactsAPI from "./utils/ContactsAPI";
 import CreateContact from "./components/create-contact/create-contact.component";
 import { Route } from "react-router-dom";
 
 function App(props) {
-  console.log(props);
   const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
-    ContactsAPI.getAll().then((contacts) => setContacts(contacts));
+    //ContactsAPI.getAll().then((contacts) => setContacts(contacts));
+    setContacts(contactsData)
   }, []);
 
   const removeContact = (contact) => {
