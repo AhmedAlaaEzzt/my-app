480fad411a7226b457fb707cd107564fadbf989d
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-16 13:07:29 +0200

    00058 - prepare demo

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import "./App.css";
 import About from "./pages/about/about.page";
 import Users from "./pages/users/users.page";
 import Home from "./pages/home/home.page";
-import { BrowserRouter,Switch,Route } from "react-router-dom";
-import {users} from './data/fack-users.js';
+import { BrowserRouter, Route } from "react-router-dom";
+import { users } from "./data/fack-users.js";
 function App() {
   return (
     <div className="">
       <BrowserRouter>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users users={users} />
+        </Route>
       </BrowserRouter>
     </div>
   );
