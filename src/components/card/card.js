d749ea79f886d3685e37544ea55afe731878dcd5
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-07-26 10:06:52 +0200

    00006 - fix the placeholer image link

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = () => {
   return (
     <div>
-      <img alt="user" src="http://placehold.it/75" />
+      <img alt="user" src="https://via.placeholder.com/75" />
       <div>
         <div>Name here...</div>
         <div>Company Name here ...</div>
